5d72123de5280df925d0541023710bba17de7aca
Karol Majta <karolmajta@gmail.com>
2013-07-19 20:40:31 +0200

    Draft of mainloop

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import pygame
+import sys
+import time
 
-def mainloop():
-    print "Hello World :)"
+
+def init_game():
+    window = pygame.display.set_mode((468, 60))
+    pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Pirate Squirrel")
+    screen = pygame.display.get_surface()
+    return (window, screen)
+
+
+def shutdown_game(window):
+    sys.exit(0)
+
+
+def mainloop(screen):
+    # flip the display
+    pygame.display.flip()
+    # we need to transform all events from
+    # big todo
+    time.sleep(2)
+
 
 if __name__ == "__main__":
-    mainloop()
+    window, screen = init_game()
+    mainloop(screen)
+    shutdown_game(window)
